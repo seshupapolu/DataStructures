c63b3f333374e7e1a209f2b4d47be4478a86aedb
Sesha_Papolu <Sesha_Papolu@EPINHYDW1014>
2017-05-09 18:08:31 +0530

    Added Code

---
+++ DataStructures.Run/Datastructures.All/LinkedList.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datastructures.All
+{
+    public class LinkedList<T>
+    {
+        public LinkedListNode<T> Head { get; private set; }
+        public LinkedListNode<T> Tail { get; private set; }
+        public int Count { get; private set; }
+
+        public void AddToFront(LinkedListNode<T> node)
+        {
+            LinkedListNode<T> temp = Head;
+            Head = node;
+            Head.Next = temp;
+            Count++;
+            if (Count == 1)
+            {
+                Tail = node;
+            }
+        }
+    }
+
+    public class LinkedListNode<T>
+    {
+        public T Value { get; set; }
+        public LinkedListNode<T> Next { get; set; }
+    }
+}
